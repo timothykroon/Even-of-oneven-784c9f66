eeb908adcd2662cc9dac8b8a63c02f32b716fc15
Timothy Kroon <timothykroon1@gmail.com>
2020-03-04 10:11:34 +0100

    Uitwerking van de exercise

---
+++ even_of_oneven.php
@@ -0,0 +1,12 @@
+<?php
+
+echo "vul een getal in" . PHP_EOL;
+$getal = readline();
+
+$ding = $getal % 2;
+
+if($ding == 0){
+    echo "het is een even getal";
+} else{
+    echo "het getal in oneven";
+}
